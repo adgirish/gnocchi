9d8109ae2f2450c6be204f69749db08cb6d23901
Kunal Gosar <gosarkunal9@gmail.com>
2017-10-02 12:26:26 -0700

    Make file for gnocchi python

--- gnocchi-apis/src/main/scala/net/fnothaft/gnocchi/api/java/JavaGnocchiSession.scala
+++ gnocchi-apis/src/main/scala/net/fnothaft/gnocchi/api/java/JavaGnocchiSession.scala
@@ -1,26 +1,29 @@
 /**
-  * Licensed to Big Data Genomics (BDG) under one
-  * or more contributor license agreements.  See the NOTICE file
-  * distributed with this work for additional information
-  * regarding copyright ownership.  The BDG licenses this file
-  * to you under the Apache License, Version 2.0 (the
-  * "License"); you may not use this file except in compliance
-  * with the License.  You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Licensed to Big Data Genomics (BDG) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The BDG licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 // (TODO) Add boilerplate for Java wrapper onto Gnocchi
 
 package net.fnothaft.gnocchi.api.java
 
+import net.fnothaft.gnocchi.primitives.phenotype.Phenotype
+import net.fnothaft.gnocchi.primitives.variants.CalledVariant
 import org.apache.spark.api.java.JavaSparkContext
+import org.apache.spark.sql.Dataset
 import org.bdgenomics.adam.rdd.ADAMContext
 import net.fnothaft.gnocchi.sql.GnocchiSession
 
@@ -30,14 +33,14 @@ object JavaGnocchiSession {
   implicit def toGnocchiSession(jgs: JavaGnocchiSession): GnocchiSession = jgs.gs
 }
 
-// (TODO) Write custom Dataset[CalledVariant] in Python file
+// (TODO) Write regressPhenotype functionality
 
 /**
  * The JavaGnocchiSession provides java-friendly functions on top of GnocchiSession.
  *
  * @param gs The GnocchiSession to wrap.
  */
- class JavaGnocchiSession(val gs: GnocchiSession) extends Serializable {
+class JavaGnocchiSession(val gs: GnocchiSession) extends Serializable {
 
   /**
    * @return Returns the Gnocchi Spark Context associated with this Java Gnocchi Session.
@@ -61,8 +64,8 @@ object JavaGnocchiSession {
   /**
    * (TODO) Add comments
    */
-  def loadGenotypes(genotypesPath: java.lang.String): Dataset[CalledVariant] = {
-    gs.loadGenotypes(genotypesPath)
+  def loadGenotypesAsText(genotypesPath: java.lang.String): Dataset[CalledVariant] = {
+    gs.loadGenotypesAsText(genotypesPath)
   }
 
   /**
@@ -73,19 +76,19 @@ object JavaGnocchiSession {
                      phenoName: java.lang.String,
                      delimiter: java.lang.String,
                      covarPath: Option[String] = None,
-                     covarNames: Option[List[String]] = None): Map[String, BetterPhenotype] = {
+                     covarNames: Option[List[String]] = None): Map[String, Phenotype] = {
     gs.loadPhenotypes(phenotypesPath,
-                      primaryID,
-                      phenoName,
-                      delimiter,
-                      covarPath,
-                      covarNames)
+      primaryID,
+      phenoName,
+      delimiter,
+      covarPath,
+      covarNames)
   }
 
   /**
-   * (TODO) Add comments
+   * (TODO) Add commentsee
    */
-  def getBetterPhenotypeByKey(betterPhenotypeMap: Map[String, BetterPhenotype], key: java.lang.String): BetterPhenotype = {
+  def getBetterPhenotypeByKey(betterPhenotypeMap: Map[String, Phenotype], key: java.lang.String): Phenotype = {
     betterPhenotypeMap(key)
   }
 }
