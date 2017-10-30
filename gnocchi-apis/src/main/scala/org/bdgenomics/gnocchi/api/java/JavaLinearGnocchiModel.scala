e7557f096a71b665ad1c5358a238edc48f80b2e9
Adithya Girish <adithyagirish@berkeley.edu>
2017-10-29 19:33:20 -0700

    working on bindings

--- gnocchi-apis/src/main/scala/org/bdgenomics/gnocchi/api/java/JavaLinearGnocchiModel.scala
+++ gnocchi-apis/src/main/scala/org/bdgenomics/gnocchi/api/java/JavaLinearGnocchiModel.scala
@@ -1,8 +1,13 @@
 package org.bdgenomics.gnocchi.api.java
 
-/**
-  * Created by adithya on 10/27/17.
-  */
-class JavaLinearGnocchiModel {
+import org.apache.spark.sql.Dataset
+import org.bdgenomics.gnocchi.models.variant.{ QualityControlVariantModel, LinearVariantModel }
+import org.bdgenomics.gnocchi.models.{ GnocchiModelMetaData, GnocchiModel, LinearGnocchiModel }
+import org.bdgenomics.gnocchi.primitives.phenotype.Phenotype
+import org.bdgenomics.gnocchi.primitives.variants.CalledVariant
 
+class JavaLinearGnocchiModel(val lgm: LinearGnocchiModel) {
+  def mergeGnocchiModel(otherModel: GnocchiModel[LinearVariantModel, LinearGnocchiModel]): GnocchiModel[LinearVariantModel, LinearGnocchiModel] = {
+    lgm.mergeGnocchiModel(otherModel)
+  }
 }
